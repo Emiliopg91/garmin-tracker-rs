8df30e65fa0c8030aa2f2e9311a885f3e1f1d995
Emiliopg91 <ojosdeserbio@gmail.com>
2026-07-05 23:19:37 +0200

    [improve] x11 flag enabled by default

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,23 +11,22 @@ struct Cli {
     verbose: bool,
 
     /// Force usage of Wayland
-    #[arg(short, long)]
-    wayland: bool,
+    #[arg(short, long, default_value_t = true)]
+    x11: bool,
 }
 
 fn main() {
-    let cli = Cli::parse();
-
-    unsafe {
-        if !cli.wayland {
-            std::env::set_var("GDK_BACKEND", "x11");
-        }
-        std::env::set_var("LOGGER_LEVEL", if cli.verbose { "Debug" } else { "" });
-    }
-
     #[cfg(debug_assertions)]
     if std::env::var("IN_DEBUG").is_err() {
         tauri_rs_ts_ipc::build();
+    }
+    let cli = Cli::parse();
+
+    unsafe {
+        if cli.x11 {
+            std::env::set_var("GDK_BACKEND", "x11");
+        }
+        std::env::set_var("LOGGER_LEVEL", if cli.verbose { "Debug" } else { "" });
     }
 
     garmin_tracker_rs_lib::run();
