c4498f8c2e7180e9119b37a1ea857b5ebf34ede0
Emiliopg91 <ojosdeserbio@gmail.com>
2026-07-27 01:36:34 +0200

    [improve] Database ID hashing/comparing

--- src-tauri/src/garmin/database/dao/exercise/mod.rs
+++ src-tauri/src/garmin/database/dao/exercise/mod.rs
@@ -1,24 +1,11 @@
-use std::hash::Hash;
-
 use garmin_tracker_rs_macros::Entity;
 
 #[derive(Clone, Debug, Default, Entity)]
+#[entity(hasheable = true, comparable = true)]
 pub struct Exercise {
     #[id]
     pub category: String,
     #[id]
     pub id: u16,
     pub name: String,
-}
-impl PartialEq for Exercise {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id && self.category == other.category
-    }
-}
-impl Eq for Exercise {}
-impl Hash for Exercise {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.id.hash(state);
-        self.category.hash(state);
-    }
 }
