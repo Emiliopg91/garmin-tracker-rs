9f6b1aae16ba91e9de5025b249f5458ace663aac
Emiliopg91 <ojosdeserbio@gmail.com>
2026-07-27 10:46:18 +0200

    [improve] USB device polling

--- src-tauri/src/ui/devices/mod.rs
+++ src-tauri/src/ui/devices/mod.rs
@@ -1,13 +1,12 @@
 pub mod models;
 
 use garmin_tracker_rs_macros::translate;
-use std::{
-    sync::{LazyLock, Mutex},
-    time::Duration,
-};
+use nusb::hotplug::HotplugEvent;
+use std::sync::{LazyLock, Mutex};
+use tokio_stream::StreamExt;
 
 use tauri::{AppHandle, Emitter, async_runtime::JoinHandle};
-use tauri_plugin_log::log::{info, warn};
+use tauri_plugin_log::log::{error, info, warn};
 
 use crate::{
     garmin::{
@@ -33,69 +32,83 @@ pub async fn start_device_watcher(app: AppHandle) {
         info!("Starting device monitor...");
         *watcher = Some(tauri::async_runtime::spawn(async move {
             let mut devices: Vec<DeviceListItem> = Vec::new();
-            info!("Device monitor initialized");
 
-            tokio::time::sleep(Duration::from_secs(1)).await;
-            loop {
-                if let Ok(cur_dev) = MTP_CLIENT_INST
-                    .lock()
-                    .await
-                    .get_connected_devices()
-                    .await
-                    .map_err(|e| e.to_string())
-                {
-                    for device in &cur_dev {
-                        if !devices
-                            .iter()
-                            .any(|e| e.serial_number == device.serial_number)
-                        {
-                            devices.push(device.clone());
+            match nusb::watch_devices() {
+                Ok(w) => {
+                    info!("Device monitor initialized");
 
-                            if let Ok(None) = Device::select_by_id(&device.serial_number) {
-                                let _ = Device::insert().item(Device::from(device)).execute();
+                    mtp_dev_check(app.clone(), &mut devices).await;
+                    let mut watch = w;
+                    while let Some(event) = watch.next().await {
+                        match event {
+                            HotplugEvent::Connected(_) | HotplugEvent::Disconnected(_) => {
+                                mtp_dev_check(app.clone(), &mut devices).await;
                             }
-
-                            let payload: DeviceListItem = device.clone();
-                            let _ = app.emit("device_connected", payload);
-
-                            info!(
-                                "Connected {} {} ({})",
-                                device.manufacturer, device.model, device.serial_number
-                            );
-                            show_notification(NotificationDefinition {
-                                title: translate!("device_connected"),
-                                body: format!("{} {}", device.manufacturer, device.model),
-                                kind: NotificationKind::Temporal,
-                            });
                         }
                     }
+                }
+                Err(e) => {
+                    error!("Could not initialize device monitor: {e}");
+                }
+            };
+        }));
+    }
+}
 
-                    for device in &devices {
-                        if !cur_dev
-                            .iter()
-                            .any(|d| d.serial_number == device.serial_number)
-                        {
-                            let payload: DeviceListItem = device.clone();
-                            let _ = app.emit("device_disconnected", payload);
+async fn mtp_dev_check(app: AppHandle, devices: &mut Vec<DeviceListItem>) {
+    if let Ok(cur_dev) = MTP_CLIENT_INST
+        .lock()
+        .await
+        .get_connected_devices()
+        .await
+        .map_err(|e| e.to_string())
+    {
+        for device in &cur_dev {
+            if !devices
+                .iter()
+                .any(|e| e.serial_number == device.serial_number)
+            {
+                devices.push(device.clone());
 
-                            info!(
-                                "Disconnected {} {} ({})",
-                                device.manufacturer, device.model, device.serial_number
-                            );
-                            show_notification(NotificationDefinition {
-                                title: translate!("device_disconnected"),
-                                body: format!("{} {}", device.manufacturer, device.model),
-                                kind: NotificationKind::Temporal,
-                            });
-                        }
-                    }
-
-                    devices
-                        .retain(|d| cur_dev.iter().any(|cd| cd.serial_number == d.serial_number));
+                if let Ok(None) = Device::select_by_id(&device.serial_number) {
+                    let _ = Device::insert().item(Device::from(device)).execute();
                 }
 
-                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                let payload: DeviceListItem = device.clone();
+                let _ = app.emit("device_connected", payload);
+
+                info!(
+                    "Connected {} {} ({})",
+                    device.manufacturer, device.model, device.serial_number
+                );
+                show_notification(NotificationDefinition {
+                    title: translate!("device_connected"),
+                    body: format!("{} {}", device.manufacturer, device.model),
+                    kind: NotificationKind::Temporal,
+                });
             }
-        }));
+        }
+
+        for device in devices.iter() {
+            if !cur_dev
+                .iter()
+                .any(|d| d.serial_number == device.serial_number)
+            {
+                let payload: DeviceListItem = device.clone();
+                let _ = app.emit("device_disconnected", payload);
+
+                info!(
+                    "Disconnected {} {} ({})",
+                    device.manufacturer, device.model, device.serial_number
+                );
+                show_notification(NotificationDefinition {
+                    title: translate!("device_disconnected"),
+                    body: format!("{} {}", device.manufacturer, device.model),
+                    kind: NotificationKind::Temporal,
+                });
+            }
+        }
+
+        devices.retain(|d| cur_dev.iter().any(|cd| cd.serial_number == d.serial_number));
     }
 }
