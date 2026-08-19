d7ddd8d5ca18c9da3f9c43e2d4c53413859ecb05
Emiliopg91 <ojosdeserbio@gmail.com>
2026-08-19 02:31:48 +0200

    [fix] Missing marker icons

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,21 @@ import App from "@/components/App/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 import "@/styles/main.css";
 import { AppProvider } from "./context/AppProvider";
+
+delete (L.Icon.Default.prototype as unknown as { _getIconUrl?: unknown })
+  ._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <AppProvider>
