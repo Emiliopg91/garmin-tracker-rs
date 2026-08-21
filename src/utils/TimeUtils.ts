92ce4d037f3089c51087f0f8ca2e255905b6f2f5
Emiliopg91 <ojosdeserbio@gmail.com>
2026-08-21 12:46:08 +0200

    [fix] Missing time in zone when 0

--- src/utils/TimeUtils.ts
+++ src/utils/TimeUtils.ts
@@ -1,5 +1,9 @@
 export class TimeUtils {
   public static formatDuration(seconds: number): string {
+    if (seconds == 0) {
+      return "0:00";
+    }
+
     const h = Math.floor(seconds / 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor(seconds % 60);
